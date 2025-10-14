ae8bbcce2be8b705f356159d60d6cd059cdc2f74
enespelko <enespelko@users.noreply.github.com>
2025-10-14 16:48:30 +0200

    Add Teacher model with properties

---
+++ SimpleWebApp/Models/Teacher.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimpleWebApp.Models;
+
+public class Teacher
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public string Subject { get; set; } = string.Empty;
+    public DateTime HireDate { get; set; }
+}
