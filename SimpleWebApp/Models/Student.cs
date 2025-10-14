0c8c629bc13dea890649bd62a5697e9a378c2874
enespelko <enespelko@users.noreply.github.com>
2025-10-14 16:47:43 +0200

    Add Student model with properties

---
+++ SimpleWebApp/Models/Student.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string FullName { get; set; } = string.Empty;
+    public DateTime EnrollmentDate { get; set; }
+    public int GradeLevel { get; set; }
+}
